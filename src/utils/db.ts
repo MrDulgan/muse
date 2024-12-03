bae0b54b25ab1c1d44cbbd6ab30f8b4a3100047c
MrDulgan <150480581+MrDulgan@users.noreply.github.com>
2024-12-03 23:18:13 +0300

    Update db.ts

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,3 +1,15 @@
-import Prisma from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
-export const prisma = new Prisma.PrismaClient();
+const prisma = new PrismaClient({
+  log: ['query', 'info', 'warn', 'error'],
+});
+
+const shutdown = async () => {
+  await prisma.$disconnect();
+  process.exit(0);
+};
+
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
+
+export default prisma;
