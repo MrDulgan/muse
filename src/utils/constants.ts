38ee877b0fc7df9f7d60e9773545e4a11e674543
MrDulgan <150480581+MrDulgan@users.noreply.github.com>
2024-12-03 23:16:55 +0300

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,5 @@
-export const ONE_HOUR_IN_SECONDS = 60 * 60;
-export const ONE_MINUTE_IN_SECONDS = 1 * 60;
+export const TIME_CONSTANTS = {
+  ONE_MINUTE: 60,
+  ONE_HOUR: 60 * 60,
+  ONE_DAY: 60 * 60 * 24,
+} as const;
