eba37fdad683642381da2544089909ceadefa347
MrDulgan <150480581+MrDulgan@users.noreply.github.com>
2024-12-03 23:14:20 +0300

    Update arrays.ts

--- src/utils/arrays.ts
+++ src/utils/arrays.ts
@@ -1,9 +1,9 @@
-export const chunk = <T>(arr: T[], len: number) => {
-  const chunks = [];
+export const chunk = <T>(arr: T[], len: number): T[][] => {
+  if (len <= 0) throw new Error("Chunk size must be greater than 0");
 
-  let i = 0;
-  while (i < arr.length) {
-    chunks.push(arr.slice(i, i += len));
+  const chunks: T[][] = [];
+  for (let i = 0; i < arr.length; i += len) {
+    chunks.push(arr.slice(i, i + len));
   }
 
   return chunks;
